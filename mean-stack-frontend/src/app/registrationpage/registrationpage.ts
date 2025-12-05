c6765246d0f362d6eed2ee430d941684dac26d9c
Jamal <jamalmohomad04@gmail.com>
2025-12-04 22:21:57 -0500

    added basic validation

--- mean-stack-frontend/src/app/registrationpage/registrationpage.ts
+++ mean-stack-frontend/src/app/registrationpage/registrationpage.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit} from '@angular/core';
 import { Crud } from '../crud';
-import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registrationpage',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './registrationpage.html',
   styleUrl: './registrationpage.css',
 })
@@ -13,18 +14,20 @@ export class Registrationpage implements OnInit{
   constructor(private crud:Crud){}
    
   form!: FormGroup;
- 
+
   ngOnInit():void{
   this.form = new FormGroup({
-     username: new FormControl(''),
-     email: new FormControl(''),
-     password: new FormControl('')
+     username: new FormControl('', Validators.required),
+     email: new FormControl('', Validators.email),
+     password: new FormControl('', Validators.required)
   });
 }
 
    addReg() {
+     if(this.form.valid) {
      this.crud.addUser(this.form.value).subscribe((response) => {
        console.log(response);
      });
-}
+     }
+  }
 }
